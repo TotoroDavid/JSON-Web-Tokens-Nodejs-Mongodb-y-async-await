e3ca26083ce955f210bfe80456b0f2dd654dd030
TotoroDavid <davidtoro65@hotmail.com>
2021-06-03 15:12:27 -0500

    JSON web tokens with nodeJs, mondodb and async

---
+++ src/controllers/authController.js
@@ -0,0 +1,50 @@
+const { Router } = require('express')
+const router = Router()
+
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+const verifyToken = require('../controllers/verifyToken')
+
+const User = require('../models/Users')
+
+router.post('/signup', async(req, res, next) => {
+    const { username, email, password } = req.body
+    const user = new User({
+        username: username,
+        email: email,
+        password: password
+    })
+    user.password = await user.encryptPassword(user.password)
+    await user.save()
+    const token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 60 * 60 * 24
+    })
+    res.json({ auth: true, token: token })
+})
+
+router.get('/me', verifyToken, async(req, res, next) => {
+    const user = await User.findById(req.userId, { password: 0 })
+    if (!user) {
+        return res.status(404).send('No user found')
+    }
+    res.json(user)
+})
+
+router.post('/signin', async(req, res, next) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ email: email })
+    if (!user) {
+        return res.status(404).send('The email does not exist')
+    }
+    const validPassword = await user.validatePassword(password)
+    if (!validPassword) {
+        return res.status(401).json({ auth: false, token: null })
+    }
+    const token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 60 * 60 * 24
+    })
+    res.json({ auth: true, token })
+})
+
+
+module.exports = router
